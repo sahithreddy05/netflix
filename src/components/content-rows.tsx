33f57a9536d93bde2fa7478d80727456deecdaf9
sahithreddy05 <sahithreddy050101@gmail.com>
2023-02-08 13:04:38 -0800

    sliding

--- src/components/content-rows.tsx
+++ src/components/content-rows.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { fetchRequest, MovieResponse, MovieResult } from '../common/api';
 import { ENDPOINT } from '../common/endpoints';
 type RowProp = {
     endpoint: string;
     title: string;
 }
+const CARD_WIDTH = 200;
 export default function ContentRows({ title, endpoint }: RowProp) {
+    const sliderRef = useRef<HTMLSelectElement>(null);
     const [rowData, setRowData] = useState<MovieResult[]>([]);
+    const [translateX, setTranslateX] = useState(0);
     async function fetchRowData() {
         const response = await fetchRequest<MovieResponse<MovieResult[]>>(
             endpoint
@@ -17,24 +20,55 @@ export default function ContentRows({ title, endpoint }: RowProp) {
         fetchRowData();
     }, []
     )
-    function createImageURL(path: string) {
-        return `${import.meta.env.VITE_BASE_IMAGE_URL}${path}`
+    function createImageURL(path: string, width: number) {
+        return `${import.meta.env.VITE_BASE_IMAGE_URL}/w${width}/${path}`
     }
-    return <section>
-        <h2 className='mb-4'>{title}</h2>
-        <section className='flex flex-nowrap overflow-x-auto'>
-            {rowData?.map(row => {
-                const { id, title, poster_path } = row;
-
-                return (
-                    <section key={id} className='h-[200px] w-[200px] flex-none'>
-                        <img 
-                        className='h-full w-full object-contain' 
-                        src={createImageURL(poster_path)} 
-                        alt={title} />
-                    </section>
-                )
-            })}
-        </section>
-    </section>
+    function onNextClick() {
+        if (sliderRef.current) {
+            let updatedTranslateX = translateX - 100;
+            sliderRef.current.style.transform = `translateX(${updatedTranslateX}%)`;
+            setTranslateX(updatedTranslateX);
+        }
+    }
+    function onPrevClick() {
+        if (sliderRef.current) {
+            let updatedTranslateX = translateX + 100;
+            sliderRef.current.style.transform = `translateX(${updatedTranslateX}%)`;
+            setTranslateX(updatedTranslateX);
+        }
+    }
+    return (
+        <section className=''>
+            <h2 className='mb-4'>{title}</h2>
+            <section className='relative flex flex-nowrap gap-2 overflow-hidden'>
+                <button
+                    className=' absolute h-full bg-black/25 z-[1]'
+                    onClick={onPrevClick}
+                >prev</button>
+                <button
+                    className='absolute h-full right-0 z-[1] bg-black/25'
+                    onClick={onNextClick}
+                >
+                    next
+                </button>
+                <section
+                    ref={sliderRef}
+                    className=' flex gap-2 transition-transform duration-700 ease-linear' >
+                    {rowData?.map(row => {
+                        const { id, title, poster_path } = row;
+                        return (
+                            <section
+                                key={id}
+                                className='aspect-square flex-none overflow-hidden rounded-md'>
+                                <img
+                                    loading='lazy'
+                                    className='h-full w-full '
+                                    src={createImageURL(poster_path, CARD_WIDTH)}
+                                    alt={title} />
+                            </section>
+                        )
+                    })}
+                </section>
+            </section>
+        </section>)
 }
